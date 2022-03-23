081994344998772de1a496d7a828a62526bf7694
Alvaro Moreno <98691922+alvaroarqt@users.noreply.github.com>
2022-03-23 14:34:30 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,40 @@
 grove.onGesture(GroveGesture.Right, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
     music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
 })
 input.onPinPressed(TouchPin.P2, function () {
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . # # .
+        # . . # .
+        . # # # .
+        `)
     music.playTone(392, music.beat(BeatFraction.Half))
 })
 grove.onGesture(GroveGesture.Left, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
     music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
 })
 input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # # # .
+        `)
     music.playTone(262, music.beat(BeatFraction.Half))
 })
